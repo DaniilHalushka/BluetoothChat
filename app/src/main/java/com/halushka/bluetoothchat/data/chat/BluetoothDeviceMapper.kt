126416f68dfc9ad9eb2c4d13075049e8b63050c6
Daniil Halushka <popolzen22@gmail.com>
2025-07-29 00:59:21 +0300

    develop: add functionality
    add mapper and androidBluetoothController with logic

---
+++ app/src/main/java/com/halushka/bluetoothchat/data/chat/BluetoothDeviceMapper.kt
@@ -0,0 +1,12 @@
+package com.halushka.bluetoothchat.data.chat
+
+import android.annotation.SuppressLint
+import com.halushka.bluetoothchat.domain.chat.BluetoothDeviceDomain
+
+@SuppressLint("MissingPermission")
+fun android.bluetooth.BluetoothDevice.toBluetoothDeviceDomain(): BluetoothDeviceDomain {
+    return BluetoothDeviceDomain(
+        name = name ?: "Unknown", // возможен null
+        address = address
+    )
+}
