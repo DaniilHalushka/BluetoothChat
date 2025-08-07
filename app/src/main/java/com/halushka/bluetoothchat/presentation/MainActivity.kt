39328a2c5b0d5e61fb3d66ce41e823fe4507328f
Daniil Halushka <popolzen22@gmail.com>
2025-08-07 22:53:44 +0300

    develop: add logic to MainActivity
    create launchers and other logic

--- app/src/main/java/com/halushka/bluetoothchat/presentation/MainActivity.kt
+++ app/src/main/java/com/halushka/bluetoothchat/presentation/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.halushka.bluetoothchat.presentation
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -15,6 +24,18 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val bluetoothManager by lazy {
+        applicationContext.getSystemService(BluetoothManager::class.java)
+    }
+    private val bluetoothAdapter by lazy {
+        bluetoothManager?.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +43,32 @@ class MainActivity : ComponentActivity() {
             BluetoothChatTheme {
                 val viewModel = hiltViewModel<BluetoothViewModel>()
                 val state by viewModel.state.collectAsState()
+
+                val enableBluetoothLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartActivityForResult()
+                ) { }
+
+                val permissionLauncher = registerForActivityResult(
+                    ActivityResultContracts.RequestMultiplePermissions()
+                ) { perms ->
+                    val canEnableBluetooth =
+                        perms[Manifest.permission.BLUETOOTH_CONNECT] == true
+
+                    if (canEnableBluetooth && !isBluetoothEnabled) {
+                        enableBluetoothLauncher.launch(
+                            Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                        )
+                    }
+                }
+
+                LaunchedEffect(key1 = true) {
+                    permissionLauncher.launch(
+                        arrayOf(
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        )
+                    )
+                }
 
                 Surface(
                     color = MaterialTheme.colorScheme.background
