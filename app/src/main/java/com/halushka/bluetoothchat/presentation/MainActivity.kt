33fd78134af817d6bb88849108aaf4d7433aacf0
Daniil Halushka <popolzen22@gmail.com>
2025-08-07 22:46:07 +0300

    develop: add DeviceScreen
    create DeviceScreen with it own elements

---
+++ app/src/main/java/com/halushka/bluetoothchat/presentation/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.halushka.bluetoothchat.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.halushka.bluetoothchat.presentation.components.DeviceScreen
+import com.halushka.bluetoothchat.ui.theme.BluetoothChatTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BluetoothChatTheme {
+                val viewModel = hiltViewModel<BluetoothViewModel>()
+                val state by viewModel.state.collectAsState()
+
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DeviceScreen(
+                        state = state,
+                        onStartScan = viewModel::startScan,
+                        onStopScan = viewModel::stopScan
+                    )
+                }
+            }
+        }
+    }
+}
