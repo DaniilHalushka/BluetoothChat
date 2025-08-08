b25f0edd7bfef408972e19e80414bf4de6820269
Daniil Halushka <popolzen22@gmail.com>
2025-08-08 23:01:20 +0300

    develop: add new functions to viewmodel
    add functions to connect devices

--- app/src/main/java/com/halushka/bluetoothchat/presentation/BluetoothUIState.kt
+++ app/src/main/java/com/halushka/bluetoothchat/presentation/BluetoothUIState.kt
@@ -5,4 +5,7 @@ import com.halushka.bluetoothchat.domain.chat.BluetoothDevice
 data class BluetoothUIState(
     val scannedDevices: List<BluetoothDevice> = emptyList(),
     val pairedDevices: List<BluetoothDevice> = emptyList(),
+    val isConnected: Boolean = false,
+    val isConnecting: Boolean = false,
+    val errorMessage: String? = null,
 )
